61d4ebcc044dbe3a8a27d26bc8af4fc706223db0
Fatima <faty.aome.rojas14@gmail.com>
2022-07-26 19:07:48 -0500

    Edicion del index

--- app.py
+++ app.py
@@ -7,28 +7,32 @@ from werkzeug.exceptions import abort
 app = Flask(__name__)
 #Declaracion de rutas 
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')
  
-@app.route('productos/')
+@app.route('/productos')
 def productos():
     return render_template('productos.html')
 
-@app.route('servicios/')
+@app.route('/servicios')
 def servicios():
     return render_template('servicios.html')
 
-@app.route('contactos/')
+@app.route('/contactos')
 def contactos():
     return render_template('contactos.html')
 
-@app.route('consultas/')
+@app.route('/consultas')
 def consultas():
     return render_template('consultas.html')
 
-@app.route('ventas/')
+@app.route('/ventas')
 def ventas():
     return render_template('ventas.html')
 
-@app.route('usuarios/')
+@app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
