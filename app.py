dcf571832e33f144f008630d97ee03a3f7151d95
Fatima <faty.aome.rojas14@gmail.com>
2022-08-03 16:09:44 -0500

    Terminando index 3.0

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 
 from flask import Flask, render_template, request, url_for, flash, redirect
 
-
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 #Declaracion de rutas 
